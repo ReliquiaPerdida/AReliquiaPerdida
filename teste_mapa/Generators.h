302b74ce58d8247d86eb796a6d31f99bc4628eec
Bruno S M <misterselectmice@gmail.com>
2025-12-16 20:04:33 -0300

    Delete teste_mapa directory

--- teste_mapa/Generators.h
+++
@@ -1,8 +0,0 @@
-#include "Global_Headers.h"
-
-
-typedef struct Point Point;
-
-Point *allocate_item (int x0, int xf, int y0, int yf, int **map, int **regions, int largest_id, int type_item);
-int how_many_itens (int level, int tam_map);
-int pick_random_item (int level);
